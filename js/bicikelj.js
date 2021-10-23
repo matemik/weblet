7af6b378568aacb87bda26891d3618232c7973ca
matemik <44001164+matemik@users.noreply.github.com>
2021-10-23 20:57:05 +0200

    adding leaflet, defining map, adding bikes

--- js/bicikelj.js
+++ js/bicikelj.js
@@ -1,11 +1,45 @@
-function initMap() {
-    let options = {
-        zoom: 14,
-        center: {
-            lat: 46.051093,
-            lng: 14.507186
+const initMap = () => {
+    // Initial info to define map
+    initLat = 46.051367;
+    initLng = 14.506542;
+    initZoom = 13;
+
+     // Create map
+    const map = L.map('map').setView([initLat, initLng], initZoom)
+
+    // Add tiles and icon
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    const attr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    const tiles = L.tileLayer(tileUrl, { attr })
+    tiles.addTo(map)
+    return map
+}
+
+const getData = (L) => {
+    // Create map
+    const map = initMap(L);
+    
+    // Create bike icon
+    const bikeIcon = L.icon({
+        iconUrl: "https://img.icons8.com/ios-glyphs/30/000000/bicycle.png",
+        iconSize: [30, 30]
+    });
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://opendata.si/promet/bicikelj/list/', true);
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            data = JSON.parse(xhr.responseText);
+            markers = data['markers']
+            Object.keys(markers).forEach((key, i) => {
+                L.marker(
+                    [markers[key].lat, markers[key].lng], 
+                    {icon: bikeIcon}).addTo(map)
+            });
+        } else {
+            console.log("Could not fetch the data")
         }
     }
-
-    let map = new google.maps.Map(document.getElementById('map'), options) 
-}
+    xhr.send();
+};
