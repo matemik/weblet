495a82b1efda6ec0e37b667fe47eab58d99b5f8d
matemik <44001164+matemik@users.noreply.github.com>
2021-10-28 18:42:34 +0200

    adding red bikes

--- js/bicikelj.js
+++ js/bicikelj.js
@@ -1,3 +1,11 @@
+const createMap = (L) => {
+    // Create map
+    const map = mapSettings(L);   
+
+    // Get data from API
+    getData(map)
+};
+
 const mapSettings = () => {
     // Initial info to define map
     initLat = 46.051367;
@@ -15,51 +23,51 @@ const mapSettings = () => {
     return map
 }
 
-const getData = async () => {
+const getData = async (map) => {
     const res = await fetch('https://opendata.si/promet/bicikelj/list/')
-    return await res.json()
+    const data = await res.json()        
+    const markers = data['markers']
+
+    // Show datetime of last update
+    lastUpdated = new Date(data.updated * 1000).toLocaleString('sl-SI')
+    document.getElementById("last-updated").innerHTML = `Last update: ${lastUpdated}`
+    
+    // Create markers on map
+    createMarkers(map, markers)
 }
 
-const createMap = (L) => {
-    // Create map
-    const map = mapSettings(L);
-    
+const createMarkers = (map, markers) => {
     // Create bike icon
-    const bikeIcon = L.icon({
-        iconUrl: "https://img.icons8.com/ios-glyphs/30/000000/bicycle.png",
+    const BikeIcon = L.Icon.extend({
         iconSize: [30, 30]
     });
 
-    const getData = async () => {
-        const res = await fetch('https://opendata.si/promet/bicikelj/list/')
-        const data = await res.json()        
-        const markers = data['markers']
+    let blackBike = new BikeIcon({iconUrl: "/img/icons8-black-bicycle-30.png"}),
+        redBike = new BikeIcon({iconUrl: "/img/icons8-red-bicycle-30.png"})
 
-        // Show datetime of last update
-        lastUpdated = new Date(data.updated * 1000).toLocaleString('sl-SI')
-        document.getElementById("last-updated").innerHTML = `Last update: ${lastUpdated}`
+    // Loop over markers
+    Object.keys(markers).forEach((key) => {
 
-        // Loop over markers
-        Object.keys(markers).forEach((key) => {
-            
-            // Add markers
-            const marker = L.marker(
-                [markers[key].lat, markers[key].lng], 
-                {icon: bikeIcon}
-            ).addTo(map)
-    
-            // Add tooltips
-            const tooltip = L.tooltip({
-                permanent: false,
-                direction: "auto",
-            }).setContent(`
-                    <p>${markers[key].address}</p>
-                    <p>Št. koles na voljo: ${markers[key]['station'].available}</p>
-                `)
-    
-            marker.bindTooltip(tooltip).openTooltip()
-            map.addLayer(marker)
-        });
-    }
-    getData()
-};
+        // Check available bikes at location
+        if (markers[key]['station'].available === "0") {
+            markerIcon = { icon: redBike }
+        } else {
+            markerIcon = { icon: blackBike }
+        }
+        
+        // Add marker
+        const marker = L.marker([markers[key].lat, markers[key].lng], markerIcon).addTo(map)
+
+        // Add tooltip
+        const tooltip = L.tooltip({
+            permanent: false,
+            direction: "auto",
+        }).setContent(`
+            <p>${markers[key].address}</p>
+            <p>Št. koles na voljo: ${markers[key]['station'].available}</p>
+        `)
+
+        marker.bindTooltip(tooltip).openTooltip()
+        map.addLayer(marker)
+    });
+}
