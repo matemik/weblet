ce6397de3869e73fc7e9a9ec95f9001dac93d5f9
matemik <44001164+matemik@users.noreply.github.com>
2021-11-06 14:34:17 +0100

    creating multiple css files, navbar reformatting, removing index and vreme html files

--- js/bicikelj.js
+++ js/bicikelj.js
@@ -1,141 +1,146 @@
 const createMap = (L) => {
-    // Create map
-    const map = mapSettings(L);   
+  // Create map
+  const map = mapSettings(L);
 
-    // Get data from API
-    getData(map)
+  // Get data from API
+  getData(map);
 };
 
 const mapSettings = () => {
-    // Initial info to define map
-    initLat = 46.051367;
-    initLng = 14.506542;
-    initZoom = 13;
+  // Initial info to define map
+  initLat = 46.051367;
+  initLng = 14.506542;
+  initZoom = 13;
 
-     // Create map
-    const map = L.map('map').setView([initLat, initLng], initZoom)
+  // Create map
+  const map = L.map("map").setView([initLat, initLng], initZoom);
 
-    // Add tiles and icon
-    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    const attr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    const tiles = L.tileLayer(tileUrl, { attr })
-    tiles.addTo(map)
-    return map
-}
+  // Add tiles and icon
+  const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  const attr =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+  const tiles = L.tileLayer(tileUrl, { attr });
+  tiles.addTo(map);
+  return map;
+};
 
 const getData = async (map) => {
-    const url = 'https://opendata.si/promet/bicikelj/list/'
+  const url = "https://opendata.si/promet/bicikelj/list/";
 
-    try {
-        const res = await fetch(url, {method: 'GET'})
-        const data = await res.json()
-        const markers = data['markers']
+  try {
+    const res = await fetch(url, { method: "GET" });
+    const data = await res.json();
+    const markers = data["markers"];
 
-        // Show datetime of last update
-        lastUpdated = new Date(data.updated * 1000).toLocaleString('sl-SI')
-        document.getElementById("last-updated").innerHTML = `Last updated: ${lastUpdated}`
-        
-        // Create markers on map
-        createMarkers(map, markers)
+    // Show datetime of last update
+    lastUpdated = new Date(data.updated * 1000).toLocaleString("sl-SI");
+    document.getElementById(
+      "last-updated"
+    ).innerHTML = `Last updated: ${lastUpdated}`;
 
-        // Draw stacked-bar chart
-        drawStackedChart(markers)
-    } catch (err) {
-        console.log(err)
-        const markers = {}
-        drawStackedChart(markers)
-    }
-}
+    // Create markers on map
+    createMarkers(map, markers);
+
+    // Draw stacked-bar chart
+    drawStackedChart(markers);
+  } catch (err) {
+    console.log(err);
+    const markers = {};
+    drawStackedChart(markers);
+  }
+};
 
 const createMarkers = (map, markers) => {
-    // Create bike icon
-    const BikeIcon = L.Icon.extend({
-        iconSize: [30, 30]
-    });
+  // Create bike icon
+  const BikeIcon = L.Icon.extend({
+    iconSize: [30, 30],
+  });
 
-    let blackBike = new BikeIcon({iconUrl: "img/icons8-black-bicycle-30.png"}),
-        redBike = new BikeIcon({iconUrl: "img/icons8-red-bicycle-30.png"})
+  let blackBike = new BikeIcon({ iconUrl: "img/icons8-black-bicycle-30.png" }),
+    redBike = new BikeIcon({ iconUrl: "img/icons8-red-bicycle-30.png" });
 
-    // Loop over markers
-    Object.keys(markers).forEach((key) => {
-
-        // Check available bikes at location
-        if (markers[key]['station'].available === "0") {
-            markerIcon = { icon: redBike }
-        } else {
-            markerIcon = { icon: blackBike }
-        }
-        
-        // Add marker
-        const marker = L.marker([markers[key].lat, markers[key].lng], markerIcon).addTo(map)
-
-        // Add tooltip
-        const tooltip = L.tooltip({
-            permanent: false,
-            direction: "auto",
-        }).setContent(`
-            <p>${markers[key].address}</p>
-            <p>Št. koles na voljo: ${markers[key]['station'].available}</p>
-        `)
-
-        marker.bindTooltip(tooltip).openTooltip()
-        map.addLayer(marker)
-    });
-}
-
-const drawStackedChart = (markers) => {
-
-    // Initialize data format
-    let data = {
-        'addresses': [],
-        'available': [],
-        'free': []
+  Object.keys(markers).forEach((key) => {
+    // Check available bikes at location
+    if (markers[key]["station"].available === "0") {
+      markerIcon = { icon: redBike };
+    } else {
+      markerIcon = { icon: blackBike };
     }
 
-    // Put data 
-    Object.keys(markers).forEach((key) => {
-        data['addresses'].push(markers[key].address)
-        data['available'].push(parseInt(markers[key].station.available))
-        data['free'].push(parseInt(markers[key].station.free))
-    })
+    // Add marker
+    const marker = L.marker(
+      [markers[key].lat, markers[key].lng],
+      markerIcon
+    ).addTo(map);
 
-    Highcharts.chart('stacked-bar', {
-        chart: {
-            type: 'bar',
-            backgroundColor: '#f0f0f0'
-        },
-        title: {
-            text: ''
-        },
-        lang: {
-            noData: "Test"
-        },
-        xAxis: {
-            categories: data['addresses']
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Število postaj'
-            }
-        },
-        legend: {
-            reversed: true
-        },
-        plotOptions: {
-            series: {
-                stacking: 'normal',
-                pontWidth: 10
-            }
-        },
-        series: [{
-            name: 'Št. koles na voljo',
-            data: data['available'],
-            color: "#9ae17b"
-        }, {
-            name: 'Št. parkirnih mest na voljo',
-            data: data['free'],
-            color: "#605a56"
-        }]
-    });
-}
+    // Add tooltip
+    const tooltip = L.tooltip({
+      permanent: false,
+      direction: "auto",
+    }).setContent(`
+            <p>${markers[key].address}</p>
+            <p>Št. koles na voljo: ${markers[key]["station"].available}</p>
+        `);
+
+    marker.bindTooltip(tooltip).openTooltip();
+    map.addLayer(marker);
+  });
+};
+
+const drawStackedChart = (markers) => {
+  // Initialize data format
+  let data = {
+    addresses: [],
+    available: [],
+    free: [],
+  };
+
+  Object.keys(markers).forEach((key) => {
+    data["addresses"].push(markers[key].address);
+    data["available"].push(parseInt(markers[key].station.available));
+    data["free"].push(parseInt(markers[key].station.free));
+  });
+
+  Highcharts.chart("stacked-bar", {
+    chart: {
+      type: "bar",
+      backgroundColor: "#f0f0f0",
+    },
+    title: {
+      text: "",
+    },
+    lang: {
+      noData: "No data to show",
+    },
+    xAxis: {
+      categories: data["addresses"],
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: "Število postaj",
+      },
+    },
+    legend: {
+      reversed: true,
+    },
+    plotOptions: {
+      series: {
+        stacking: "normal",
+        pontWidth: 10,
+      },
+    },
+    series: [
+      {
+        name: "Št. koles na voljo",
+        data: data["available"],
+        color: "#9ae17b",
+      },
+      {
+        name: "Št. parkirnih mest na voljo",
+        data: data["free"],
+        color: "#605a56",
+      },
+    ],
+  });
+};
